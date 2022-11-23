d3732441e4d1ea0719dccde38c6272b8da6e4a0c
yarin1994 <yarin1994@gmail.com>
2022-11-23 17:26:49 +0200

    mogngoose validation

--- BackEnd/api/models/order.js
+++ BackEnd/api/models/order.js
@@ -1,9 +1,9 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const orderSchema = mongoose.Schema({
-//     _id: mongoose.Types.ObjectId,
-//     name: String,
-//     price: Number
-// });
+const orderSchema = mongoose.Schema({
+    _id: mongoose.Types.ObjectId,
+    product: {type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true},
+    quantity: {type: Number, default: 1 }
+});
 
-// module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model('Order', orderSchema);
